e92c9c0a2176fc62a8e8dbc6daa765acd0428e1f
Adnan525 <adnaan525@gmail.com>
2023-02-18 00:19:06 +1100

    base class added

--- main.py
+++ main.py
@@ -153,6 +153,33 @@ class Pipe:
 
 # end of pipe class
 
+#base class
+class Base:
+    VEL = 5
+    WIDTH = BASE_IMG.get_width()
+    IMG = BASE_IMG
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
+# end of base class
+
+# main script
 def drawWindow(win, bird:Bird):
     win.blit(BG_IMG, (0,0))
     bird.draw(win)
